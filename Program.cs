dd881ea944f66c288bceca59bceb33b6de8cf3c0
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-05-18 23:02:19 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿decimal salariohora, salario, salariohoraextra, salariohoraextratotal, salariototal;
+int hora, horaextra;
+
+Console.Clear();
+
+Console.WriteLine("Digite seu salário-hora base:");
+salariohora = Convert.ToDecimal(Console.ReadLine()!);
+
+Console.WriteLine("Digite seu total de horas trabalhadas:");
+hora = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine("Digite sua quantidade de horas-extras trabalhadas:");
+horaextra = Convert.ToInt32(Console.ReadLine()!);
+
+calculaSalario(salariohora, hora, horaextra);
+
+Console.Clear();
+
+Console.WriteLine("--- Salário ---");
+
+Console.WriteLine("");
+
+Console.Write("Salário-hora: ");
+corVerde();
+Console.WriteLine($"{salariohora:C2}");
+Console.ResetColor();
+
+Console.Write("Horas trabalhadas: ");
+corVerde();
+Console.WriteLine($"{hora}h");
+Console.ResetColor();
+
+Console.Write("Bônus por horas-extras: ");
+corVerde();
+Console.WriteLine($"{salariohoraextra:C2}");
+Console.ResetColor();
+
+Console.Write("Total de horas-extras: ");
+corVerde();
+Console.WriteLine($"{horaextra}h");
+Console.ResetColor();
+
+Console.WriteLine("");
+
+Console.Write("Salário final: ");
+corVerde();
+Console.WriteLine($"{salariototal:C2}");
+Console.ResetColor();
+
+void corVerde()
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+}
+
+decimal calculaSalario(decimal salariohora, decimal hora, decimal horaextra)
+{
+    salario = salariohora * hora;
+
+    salariohoraextra = salariohora * 0.40m;
+
+    salariohoraextratotal = salariohoraextra * horaextra;
+
+    salariototal = salario + salariohoraextratotal;
+
+    return salariototal;
+}
